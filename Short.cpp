185c935f8c9df30ca57b4f7b855b6756becd578a
Tamjid Alam Taj <155377692+Tamjid-Alam-Taj@users.noreply.github.com>
2024-11-05 00:35:12 +0600

    Add files via upload

---
+++ Short.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool comp(int a, int b) {
+    return a < b;//ascending order
+}
+
+int main() {
+    int len;
+    cout << "Enter the number of elements: ";
+    cin >> len;
+
+    int arr[len];
+    cout << "Enter values for the array elements:\n";
+    for (int i = 0; i < len; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+
+    cout << "\nArray before sorting:\n";
+    for (int i = 0; i < len; i++) {
+        cout << arr[i] << endl;
+    }
+
+
+    sort(arr, arr + len, comp);
+
+    cout << "\nArray after sorting:\n";
+    for (int i = 0; i < len; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
